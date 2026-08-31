deb84acd402a07f065e8a62a31c429af297b30b6
Elian Echavarria <elianstanly431@gmail.com>
2026-08-30 23:09:39 -0400

    Implemented Dock apps animation using gsap when hovering

--- components/Dock.jsx
+++ components/Dock.jsx
@@ -3,8 +3,10 @@
 import React from 'react'
 import LiquidGlass from "@/components/LiquidGlass"
 import dockApps from "@/dockApp"
-import { Tooltip } from 'react-tooltip'
+// import { Tooltip } from 'react-tooltip'
 import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+import gsap from "gsap"
 
 const Dock = () => {
     const dockRef = useRef(null);
@@ -13,8 +15,49 @@ const Dock = () => {
         const dock = dockRef.current;
         if (!dock) return;
 
-        const icons = dock.querySelectorAll(".dock-icon")
-    })
+        const icons = dock.querySelectorAll(".dock-icon");
+
+        const animateIcons = (mouseX) => {
+            const { left } = dock.getBoundClientRect();
+
+            icons.forEach((icon) => {
+                const { left: iconsLeft, width } = icons.getBoundClientRect();
+                const center = iconsLeft - left + width / 2;
+                const distance = Math.abs(mouseX - center);
+
+                const intensity = Math.exp(-(distance ** 2.5) / 20000);
+
+                gsap.to(icon, {
+                    scale: 1 + 0.25 * intensity,
+                    y: -15 * intensity,
+                    duration: 0.2,
+                    ease: "power1.out",
+                })
+            });
+        };
+
+        const handleMouseMove = (e) => {
+            const { left } = dock.getBoundClientRect();
+
+            animateIcons(e.clientX - left);
+
+        };
+
+        const resetIcons = () => icons.forEach((icon) => {
+            gsap.to(icon, {
+                scale: 1, y: 0, duration: 0.3, ease: "power1.out",
+            })
+        });
+
+        dock.addEventListener('mouseover', handleMouseMove);
+        dock.addEventListener('mouseleave', resetIcons);
+
+        return () => {
+            dock.removeEventListener("mousemove", handleMouseMove);
+            dock.removeEventListener("mouseleave", resetIcons)
+        };
+    }, []);
+
 
     const toggleApp = (app) => {
         if (!app.canOpen) return;
